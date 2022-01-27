ccc8a3ec4fd1d0a01286097f54a45e454c801c5a
mindyvquach <98485408+mindyvquach@users.noreply.github.com>
2022-01-27 15:18:22 -0800

    hello world

---
+++ hello world/sketch.js
@@ -0,0 +1,18 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 2000);
+
+  textSize(48);
+  textAlign(LEFT);
+
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0, 255, 255);
+
+  fill(0);
+  text('Hello, World', width/2, height/2); 
+}
